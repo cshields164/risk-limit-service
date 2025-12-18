3499cc8dd0a58b755dc560d4f5806060701f0fb9
cshields164 <cshields164@gmail.com>
2025-12-18 17:41:47 -0500

    chore: bootstrap spring boot project

---
+++ src/main/java/com/example/demo/RiskLimitServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RiskLimitServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RiskLimitServiceApplication.class, args);
+	}
+
+}
